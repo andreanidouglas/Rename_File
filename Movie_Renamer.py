6bf311ac60d35e5b8d4f5550debccdfed0264283
Douglas R Andreani <andrenai.dr@gmail.com>
2014-12-22 22:19:22 -0200

    First Commit

---
+++ Movie_Renamer.py
@@ -0,0 +1,33 @@
+import shutil
+import os
+def file_renamer(file_path, dest):
+    file_canonical = file_path.split('\\')
+    file = file_canonical[len(file_canonical)-1]
+    
+
+    file_name = file.split('.')
+    changed_name = ''
+    for part_index in range( len(file_name) -1):
+        if part_index == len(file_name) -2:
+            changed_name += file_name[part_index]
+        else:
+            changed_name += file_name[part_index] + '_'
+
+
+    path = ''
+    for path_index in range( len(file_canonical)-1):
+        path += file_canonical[path_index] + '\\'
+
+    if dest == '':
+        return path  + changed_name + '.' + file_name[len(file_name)-1]
+    return dest + '\\' + changed_name + '.' + file_name[len(file_name)-1]
+
+def file_copy(path_src, path_dst):
+    os.chdir(path_src)
+    file_list = os.listdir(path_src)
+    for file in file_list:
+        new_file_name = file_renamer(file, path_dst)
+        shutil.copyfile(file, new_file_name)
+
+
+file_copy("Z:\\Downloads\\arrow_s01_un", 'C:\\Temp\\Arrow')
